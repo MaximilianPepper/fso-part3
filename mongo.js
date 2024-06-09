201177b00021dbce1c1ea5a0547c0510cbc00d44
MaximilianPepper <164947441+MaximilianPepper@users.noreply.github.com>
2024-06-09 16:38:29 +0200

    es 3.12 (all of it is in mongo.js)

---
+++ mongo.js
@@ -0,0 +1,49 @@
+// command line db es 3.12
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongoose = require("mongoose");
+
+const password = process.argv[2];
+const url = `mongodb+srv://fullstack:${password}@cluster0.g8xyl1a.mongodb.net/persons?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+// argv logic
+
+if (process.argv.length === 3) {
+  Person.find({}).then((persons) => {
+    persons.forEach((person) => console.log(person.name + " " + person.number));
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+  person.save().then((result) => {
+    console.log("person saved!");
+    mongoose.connection.close();
+  });
+} else {
+  console.log("usage: node mongo.js password name number");
+  process.exit(1);
+}
+// non-argv code commented out
+/*const person = new Person({
+  name: "Max",
+  number: "331-9933333",
+});
+
+person.save().then((result) => {
+  console.log("person saved!");
+  mongoose.connection.close();
+}); */
---
+++ readme.md
@@ -0,0 +1,16 @@
+### full stack phonebook
+
+live link deployed with render: [Phonebook app](https://phonebook-3r6w.onrender.com/)
+
+#### front-end
+
+react
+
+#### backend
+
+node.js
+
+#### middleware
+
+express <br>
+morgan
