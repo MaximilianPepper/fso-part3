d31eee0b7fa2dd6eaadd969826c5fb01ceb1c08f
MaximilianPepper <164947441+MaximilianPepper@users.noreply.github.com>
2024-06-09 20:55:32 +0200

    3.15 - 3.16

--- index.js
+++ index.js
@@ -17,35 +17,15 @@ app.use(
   morgan(":method :status :res[content-length] - :response-time ms :body")
 );
 
-/*let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];*/
-
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then((persons) => {
-    response.json(persons);
-  });
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      response.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
+// this currently doesnt work because it doesnt interact with db
 app.get("/info", (request, response) => {
   const data = `Phonebook has info for ${
     persons.length
@@ -54,20 +34,21 @@ app.get("/info", (request, response) => {
   console.log("test");
 });
 
+// outdated
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   const person = persons.find((p) => p.id === id);
   person ? response.json(person) : response.status(404).end();
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then((res) => response.status(204).end())
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const { name, number } = request.body;
   const person = new Person({ name: name, number: number });
   if (!name || !number) {
@@ -75,19 +56,24 @@ app.post("/api/persons", (request, response) => {
       error: "content missing",
     });
   }
-  //const [people] = persons.map((person) => person.name.toLowerCase());
 
-  //if (people.includes(name.toLowerCase()))
-  //return response.status(400).json({ error: "name must be unique" });
-
-  person.save().then((newPerson) => response.json(newPerson));
+  person
+    .save()
+    .then((newPerson) => response.json(newPerson))
+    .catch((error) => next(error));
 });
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-function getId() {
-  return Math.floor(Math.random() * 123456);
-}
